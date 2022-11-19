faefc01f03938eb58b795ee0f1a707169c293191
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-19 03:18:05 -0300

    import assest and global css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import PageLayoud from '../component/PageLayoud'
 import styles from '../styles/Home.module.css'
@@ -7,7 +5,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <PageLayoud title='Home'>
+      <PageLayoud 
+        title='Welcome To Galaxy!'
+        content='Iniciar portfolio'
+      >
 
         <Link href={'/Home'}><h1>Home</h1></Link>
 
