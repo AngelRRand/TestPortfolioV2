d7c682f7abc9fd04b572afad465d4e753c48c497
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-19 03:00:50 -0300

    config app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AnimatePresence } from 'framer-motion'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <>
+      <AnimatePresence exitBeforeEnter initial={false}>
+
+        <Component {...pageProps} key={router.asPath}/>
+
+      </AnimatePresence>
+    </>
+  )
 }
