9ed53c1a99d8e05e7a606437c12e514cf59b2785
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-19 03:58:29 -0300

    fixed starts

--- component/Stars.tsx
+++ component/Stars.tsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import StarsIMG from "../assets/Stars.gif"
-import styles from './PageLayoud.module.css'
+import styles from './Stars.module.css'
 
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+/* import { useRouter } from 'next/router' */
 
 const Stars = ({ }) => {
-    const router = useRouter()
-    console.log(router.asPath)
-    return (
-        <div>
-            <Image
-                src={StarsIMG}
-                alt='Stars background web'
-                className={`${styles.stars}`}
-            />
-        </div>
-    )
+    /* const router = useRouter()
+    let currentRouter = router.asPath
+     */
+        return <Image
+            src={StarsIMG}
+            alt='Stars background web'
+            className={styles.stars}
+        />
 }
 
 export default Stars
