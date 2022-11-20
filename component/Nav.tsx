775ca9775019f3698e66dba12bef9a80548be2a0
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-20 13:02:21 -0300

    conditions render nav component

--- component/Nav.tsx
+++ component/Nav.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import styles from './Nav.module.css';
+import { useRouter } from 'next/router'
 
 const Nav = () => {
-  return (
-    <div className='CAJA'>
-        <h1>NAV</h1>
-    </div>
-  )
+
+    const router = useRouter()
+    let currentRouter = router.asPath
+
+    
+    if(currentRouter === '/'){
+        return <></>
+    }else{
+        return (
+            <header className='CAJA'>
+                <nav>NAV</nav>
+            </header>
+        )
+    }
 }
 
 export default Nav
