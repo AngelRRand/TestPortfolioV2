b467760ee0b398aeb4b22ea0006292922df6135c
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-20 12:58:43 -0300

    new component

---
+++ component/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='CAJA'>
+        <h1>NAV</h1>
+    </div>
+  )
+}
+
+export default Nav
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
+import Nav from '../component/Nav'
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
-      <AnimatePresence exitBeforeEnter initial={false}>
+      <AnimatePresence mode='wait'  initial={false}>
 
         <Component {...pageProps} key={router.asPath}/>
-
+        <Nav></Nav>
       </AnimatePresence>
     </>
   )
