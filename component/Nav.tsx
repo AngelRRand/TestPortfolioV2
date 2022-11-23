0bae7afa4612f180c2b8e0011b295f3e7e6ef606
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-23 00:30:10 -0300

    import images nav

--- component/Nav.tsx
+++ component/Nav.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { motion } from "framer-motion";
-import ligthgreen from '../../assets/Nav/ligthgreen.gif';
+import ligthgreen from '../assets/Nav/ligthgreen.gif';
 import ligthred from '../../assets/Nav/ligthred.gif';
 
 import Link from 'next/link'
@@ -33,8 +33,20 @@ const Nav = () => {
                 <nav className={styles.nav}>
                     <h3 className='pointer'>MENU</h3>
                     <ul>
-                        <li className={styles.navLink}> <Link href={'/Home'}><p>To Galaxy</p></Link></li>
-                        <li className={styles.navLink}> <Link href={url}><p>Go to Planet</p></Link></li>
+                        <li className={styles.navLink}> <Link href={'/Home'}><p>To Galaxy <Image
+                            src={ligthgreen}
+                            alt='Planet Terra galaxy link image'
+                            className='pointer'
+                            width={30}
+                            height={30}
+                        /></p></Link></li>
+                        <li className={styles.navLink}> <Link href={url}><p>Go to Planet <Image
+                            src={ligthgreen}
+                            alt='Planet Terra galaxy link image'
+                            className='pointer'
+                            width={30}
+                            height={30}
+                        /></p></Link></li>
                     </ul>
                 </nav>
             </header>
