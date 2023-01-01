4acd96d42d2ab34e9b7e916f17d32e3efc490785
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-12-31 22:03:35 -0300

    asdasd

--- component/Avatar.tsx
+++ component/Avatar.tsx
@@ -3,23 +3,7 @@ import React from 'react'
 const Avatar = () => {
   return (
     <div>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-
-      <h1>Feliz navidad</h1>
-      <h1>Feliz navidad</h1>
-
-      <h1>Feliz navidad</h1>
-
-      <h1>Feliz navidad</h1>
+      <h1>Feliz año nuevo</h1>
 
     </div>
   )
